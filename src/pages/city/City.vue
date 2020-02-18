d7e1cbfb8142870e8121e53bde7f17fee3fe9ebd
zengcanhuang <zengcanhuang@163.com>
2020-02-19 01:38:52 +0800

    完成8-8

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,27 +1,63 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search
+    :cities="cities"
+    ></city-search>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"
+    ></city-list>
+    <city-alphabet
+    :cities="cities"
+    @change="handleLetterChange"
+    ></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search.vue'
+import CityList from './components/List.vue'
+import CityAlphabet from './components/Alphabet.vue'
 
   export default {
     name: 'City',
     components:{
       CityHeader,
-      CitySearch
+      CitySearch,
+      CityList,
+      CityAlphabet
     },
     data(){
       return {
-
+         cities: {},
+         hotCities: [],
+         letter: ''
       }
     },
     methods: {
+      getCityInfo: function(){
+        axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+      },
+      handleGetCityInfoSucc: function(res){
+        res = res.data
+        if(res.ret && res.data){
+          const data = res.data
+           this.cities = data.cities
+           this.hotCities = data.hotCities
+        }
+      },
+      handleLetterChange: function(letter){
 
+        this.letter = letter
+      }
+    },
+    mounted() {
+      this.getCityInfo();
     }
   }
 </script>
